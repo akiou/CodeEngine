ffa961186545ddbf16abd458ba10ab88218acd9b
Akio Oka <tenniswokya7117@gmail.com>
2025-02-05 23:45:10 -0800

    implement callback api with logging

--- wxo-mock-server/main.py
+++ wxo-mock-server/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from logging import getLogger
 
+from fastapi import FastAPI, Request
+
+
+logger = getLogger("uvicorn.app")
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.post("/callback/{correlation_id}")
+async def callback_test_mock(request: Request, correlation_id: str, json_data: dict):
+    logger.info("callback_test_mock API is invoked.")
+    headers = request.headers
+    logger.info(f"{correlation_id=}")
+    logger.info(f"{headers=}")
+    logger.info(f"{json_data=}")
+    return {"success": True}
