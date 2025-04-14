f015a6a4187b890792ddd4bf5e94086e09fbe38a
Akio Oka <tenniswokya7117@gmail.com>
2025-04-14 16:59:54 -0700

    add parroting back APIs

--- wxo-mock-server/main.py
+++ wxo-mock-server/main.py
@@ -18,3 +18,19 @@ async def callback_test_mock(request: Request, correlation_id: str, json_data: d
     logger.info(f"{headers=}")
     logger.info(f"{json_data=}")
     return {"success": True}
+
+@app.get("/parroting_back")
+async def parroting_back_get(request: Request, json_data: dict):
+    logger.info("parroting_back GET API is invoked.")
+    headers = request.headers
+    logger.info(f"{headers=}")
+    logger.info(f"{json_data=}")
+    return json_data
+
+@app.post("/parroting_back")
+async def parroting_back_post(request: Request, json_data: dict):
+    logger.info("parroting_back POST API is invoked.")
+    headers = request.headers
+    logger.info(f"{headers=}")
+    logger.info(f"{json_data=}")
+    return json_data
